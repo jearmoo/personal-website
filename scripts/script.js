7b4bcf8830adf2bb55615428e3fec2fc050cc47c
jearmoo <tjong1998@gmail.com>
2018-08-21 01:50:18 -0700

    more touchups

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
 const SCROLL_BUTTON_THRESHOLD = 100;
-const SCROLL_TIME_MS = 1500
+const SCROLL_TIME_MS = 1500;
+const FOX_NEXT_TOP_RIGHT = [0,60,0,60];
+const FOX_NEXT_NEXT_BOTTOM_LEFT = [0,50,25,55];
 
 $(document).ready(function(){
-
     /* SCROLL UP BUTTON */
     $(window).scroll(function(){
         $("#compact-navbar").slideUp();
@@ -56,24 +57,60 @@ $(document).ready(function(){
         }
     });
 
+    function randomNumber(lo,hi) {
+        return (Math.floor(Math.random() * (hi-lo+1)) + lo).toString() ;
+    }
+
+    function randomCSSTopRight(topRight,topOffset,rightOffset) {
+        const top = randomNumber(topRight[0], topRight[1]) + "%";
+        const right = randomNumber(topRight[2], topRight[3]) + "%";
+        return [
+            {
+                top: top,
+                right: right
+            },
+            {
+                top: "calc(" + top + " + " + topOffset + "px)",
+                right: "calc(" + right + " + " + rightOffset + "px)"
+            }
+        ];
+    }
+
+    function randomCSSBottomLeft(bottomLeft,bottomOffset,leftOffset) {
+        const bottom = randomNumber(bottomLeft[0], bottomLeft[1]) + "%";
+        const left = randomNumber(bottomLeft[2], bottomLeft[3]) + "%";
+        return [
+            {
+                bottom: bottom,
+                left: left
+            },
+            {
+                bottom: "calc(" + bottom + " + " + bottomOffset + "px)",
+                left: "calc(" + left + " + " + leftOffset + "px)"
+            }
+        ];
+    }
+
+
     /* FOX HUNT */
     $("#fox-start").one('click', function(){
+        const foxNextPositions = randomCSSTopRight(FOX_NEXT_TOP_RIGHT,0,30);
         $("#fox-start-speak").slideDown(null, function() {
             $(this).delay(9000).fadeOut();
             $("#fox-start").delay(10000).fadeOut();
-            $("#fox-next").delay(11000).fadeIn();
+            $("#fox-next").css(foxNextPositions[0]).delay(11000).fadeIn();
         });
 
+        const foxNextNextPositions = randomCSSBottomLeft(FOX_NEXT_NEXT_BOTTOM_LEFT,30,0);
         $("#fox-next").one('click', function(){
-            $("#fox-next-speak").slideDown(null, function() {
+            $("#fox-next-speak").css(foxNextPositions[1]).slideDown(null, function() {
                 $(this).delay(9000).fadeOut();
                 $("#fox-next").delay(10000).fadeOut();
-                $("#fox-next-next").delay(11000).fadeIn();
+                $("#fox-next-next").css(foxNextNextPositions[0]).delay(11000).fadeIn();
             });
 
-
             $("#fox-next-next").one('click', function(){
-                $("#fox-next-next-speak").slideDown(null, function() {
+                $("#fox-next-next-speak").css(foxNextNextPositions[1]).slideDown(null, function() {
                     $(this).delay(9000).fadeOut();
                     $("#fox-next-next").delay(10000).fadeOut();
                     setTimeout(function(){
@@ -85,6 +122,7 @@ $(document).ready(function(){
                         });
                         egg.listen();
                     }, 11000)
+                    $("#fox-hard-hint").delay(11000).fadeIn();
                 });
             });
         });
