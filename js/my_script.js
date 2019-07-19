7520dd877d5ff103982f5a60317f32d3038163fd
jearmoo <tjong1998@gmail.com>
2019-07-19 02:23:25 -0700

    updated for summer 2019

--- js/my_script.js
+++ js/my_script.js
@@ -5,23 +5,23 @@ const SCROLL_BUTTON_THRESHOLD = 100;
 const SCROLL_TIME_MS = 1500;
 
 /* percent ranges for positions of hidden foxes */
-const FOX_NEXT_TOP_RIGHT = [0,60,0,60];
-const FOX_NEXT_NEXT_BOTTOM_LEFT = [0,50,25,55];
-const FOX_NEXT_SPEECH_OFFSET = [0,30];
-const FOX_NEXT_NEXT_SPEECH_OFFSET = [30,0];
+const FOX_NEXT_TOP_RIGHT = [0, 60, 0, 60];
+const FOX_NEXT_NEXT_BOTTOM_LEFT = [0, 50, 25, 55];
+const FOX_NEXT_SPEECH_OFFSET = [0, 30];
+const FOX_NEXT_NEXT_SPEECH_OFFSET = [30, 0];
 
 /* fox delay times */
 
 const FOX_DELAY = 6000;
 const FOX_SPEECH_DELAY_OFFSET = 1000;
 const FOX_NEXT_DELAY_OFFSET = 1000;
-const FOX_SPEECH_DELAY = FOX_DELAY-FOX_SPEECH_DELAY_OFFSET;
+const FOX_SPEECH_DELAY = FOX_DELAY - FOX_SPEECH_DELAY_OFFSET;
 const FOX_NEXT_DELAY = FOX_DELAY + FOX_NEXT_DELAY_OFFSET;
 const FINAL_FOX_TIME = 5000;
 
-$(document).ready(function(){
+$(document).ready(function () {
     /* SCROLL UP BUTTON */
-    $(window).scroll(function(){
+    $(window).scroll(function () {
         $("#compact-navbar").slideUp();
         const up = $("#up");
         const scrollTop = $(window).scrollTop();
@@ -32,14 +32,14 @@ $(document).ready(function(){
             up.fadeOut();
         }
     })
-    $("#up").click(function(){
+    $("#up").click(function () {
         $('html, body').animate({
             scrollTop: 0
         }, SCROLL_TIME_MS, 'easeOutCubic');
     })
 
     /* RESPONSIVE NAVBAR MENU ICON SHOW AND HIDE */
-    $("#navbar-menu-icon").click(function(){
+    $("#navbar-menu-icon").click(function () {
         const cn = $("#compact-navbar");
         if (cn.is(":visible")) {
             cn.slideUp();
@@ -62,7 +62,7 @@ $(document).ready(function(){
 
     /* PROJECTS PICKER FADER */
     $(".projects-picker").imagepicker({
-        changed: function(select, newValues) {
+        changed: function (select, newValues) {
             const selectedValue = newValues[0];
             const activeClass = "project-active";
             $("." + activeClass).removeClass(activeClass);
@@ -74,8 +74,8 @@ $(document).ready(function(){
     });
 
     /* FOX HUNT */
-    function randInt(lo,hi) {
-        return (Math.floor(Math.random() * (hi-lo+1)) + lo).toString() ;
+    function randInt(lo, hi) {
+        return (Math.floor(Math.random() * (hi - lo + 1)) + lo).toString();
     }
 
     /* generate css for random position for a fox and its speech bubble */
@@ -94,30 +94,30 @@ $(document).ready(function(){
         ];
     }
 
-    $("#fox-start").one('click', function(){
-        const foxNextPositions = randomPosCSS(FOX_NEXT_TOP_RIGHT,'top','right',FOX_NEXT_SPEECH_OFFSET);
-        $("#fox-start-speak").slideDown(null, function() {
+    $("#fox-start").one('click', function () {
+        const foxNextPositions = randomPosCSS(FOX_NEXT_TOP_RIGHT, 'top', 'right', FOX_NEXT_SPEECH_OFFSET);
+        $("#fox-start-speak").slideDown(null, function () {
             $(this).delay(FOX_SPEECH_DELAY).fadeOut();
             $("#fox-start").delay(FOX_DELAY).fadeOut();
             $("#fox-next").css(foxNextPositions[0]).delay(FOX_NEXT_DELAY).fadeIn();
         });
 
-        const foxNextNextPositions = randomPosCSS(FOX_NEXT_NEXT_BOTTOM_LEFT,'bottom','left',FOX_NEXT_NEXT_SPEECH_OFFSET);
-        $("#fox-next").one('click', function(){
-            $("#fox-next-speak").css(foxNextPositions[1]).slideDown(null, function() {
+        const foxNextNextPositions = randomPosCSS(FOX_NEXT_NEXT_BOTTOM_LEFT, 'bottom', 'left', FOX_NEXT_NEXT_SPEECH_OFFSET);
+        $("#fox-next").one('click', function () {
+            $("#fox-next-speak").css(foxNextPositions[1]).slideDown(null, function () {
                 $(this).delay(FOX_SPEECH_DELAY).fadeOut();
                 $("#fox-next").delay(FOX_DELAY).fadeOut();
                 $("#fox-next-next").css(foxNextNextPositions[0]).delay(FOX_NEXT_DELAY).fadeIn();
             });
 
-            $("#fox-next-next").one('click', function(){
-                $("#fox-next-next-speak").css(foxNextNextPositions[1]).slideDown(null, function() {
+            $("#fox-next-next").one('click', function () {
+                $("#fox-next-next-speak").css(foxNextNextPositions[1]).slideDown(null, function () {
                     $(this).delay(FOX_SPEECH_DELAY).fadeOut();
                     $("#fox-next-next").delay(FOX_DELAY).fadeOut();
-                    setTimeout(function(){
+                    setTimeout(function () {
                         var egg = new Egg();
-                        egg.addCode("up,up,down,down,left,right,left,right,b,a", function() {
-                            $('#fox-hard-container').fadeIn(null, function() {
+                        egg.addCode("up,up,down,down,left,right,left,right,b,a", function () {
+                            $('#fox-hard-container').fadeIn(null, function () {
                                 $(this).delay(FINAL_FOX_TIME).fadeOut();
                             }).css('display', 'flex');
                         });
@@ -128,4 +128,4 @@ $(document).ready(function(){
             });
         });
     });
- });
+});
